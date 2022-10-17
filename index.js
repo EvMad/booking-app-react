5873ae806185085213deae1b70074d78cb9bcda5
GitPit <rifflifter@gmail.com>
2022-10-17 14:19:54 -0400

    app.get("/", (req, res) => {

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ mongoose.connection.on("connected", () => {
     console.log("mongoDB connected!");
 });
 
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
 app.listen(8800, () => {
     connect();
     console.log("Connected to backend.");
