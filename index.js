6e8fbdd443ef50e2a037a6d7a8e57562b5e419f0
GitPit <rifflifter@gmail.com>
2022-10-17 14:06:39 -0400

    npm i mongoose / await mongoose.connect

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // const express = require("express");
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
+
+try {
+    await mongoose.connect(process.env.MONGO);
+} catch (error) {
+    handleError(error);
+};
 
 app.listen(8800, () => {
     console.log("Connected to backend.")
