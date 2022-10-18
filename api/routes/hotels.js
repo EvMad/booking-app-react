3b7899869a66ab2680c48d04f70d3aa83466531a
GitPit <rifflifter@gmail.com>
2022-10-18 12:06:13 -0400

    completed router.put for hotels update by id

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -24,7 +24,7 @@ router.put("/:id", async (req, res) => {
 
     try{
         const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body } );
-        res.status(200).json(savedHotel);
+        res.status(200).json(updatedHotel);
     }catch(err){
         res.status(500).json(err);
     }
