4dc64121841881af3aa6e2c66f11398a01e8b7f6
GitPit <rifflifter@gmail.com>
2022-10-19 12:41:37 -0400

    next(err);

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -5,6 +5,6 @@ export const createHotel = async (req,res,next) => {
         const savedHotel = await newHotel.save();
         res.status(200).json(savedHotel);
     } catch (err) {
-        res.status(500).json(err);
+        next(err);
     }
 };
