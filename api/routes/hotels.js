fc7d403da39bf62a589d4b98c6ea1123e6eda08a
GitPit <rifflifter@gmail.com>
2022-10-19 12:39:09 -0400

    moved createHotel to controllers/hotel.js

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -7,14 +7,14 @@ const router = express.Router();
 
 router.post("/", async (req, res) => {
 
-    const newHotel = new Hotel(req.body);
+    // const newHotel = new Hotel(req.body);
 
-    try{
-        const savedHotel = await newHotel.save();
-        res.status(200).json(savedHotel);
-    }catch(err){
-        res.status(500).json(err);
-    }
+    // try{
+    //     const savedHotel = await newHotel.save();
+    //     res.status(200).json(savedHotel);
+    // }catch(err){
+    //     res.status(500).json(err);
+    // }
 
 });
 
