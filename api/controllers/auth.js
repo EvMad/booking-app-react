2f077d0a728942eb5dbbd33009cc2cafb883d9d8
GitPit <rifflifter@gmail.com>
2022-10-19 14:25:31 -0400

    username:req.body.username, etc / await use.save

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,8 +3,15 @@ import User from "../models/User.js";
 export const register = async (req,res,next) => {
 
     try {
-        const newUser = new User
+        const newUser = new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:req.body.passoword,
 
+        })
+
+        await newUser.save();
+        
     } catch(err){
         next(err);
     }
