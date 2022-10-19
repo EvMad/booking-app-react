9b7cf2199a1142f0ca48b947df8f4d27eeb11b4b
GitPit <rifflifter@gmail.com>
2022-10-19 13:55:08 -0400

    moved updateHotel to controllers / export const

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,25 +1,17 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
 import { createHotel } from "../controllers/hotel.js";
+import { updateHotel } from "../controllers/hotel.js";
 
 const router = express.Router();
 
 //CREATE
 
-router.post("/", createHotel);
+router.post("/", createHotel );
 
 //UPDATE
 
-router.put("/:id", async (req, res) => {
-
-    try{
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true } );
-        res.status(200).json(updatedHotel);
-    }catch(err){
-        res.status(500).json(err);
-    }
-
-});
+router.put("/:id", updateHotel );
 
 //DELETE
 
