99d2bf4e492e1f2264feb27f51c25e8a67bcfa97
GitPit <rifflifter@gmail.com>
2022-10-20 11:02:01 -0400

    export const verifyUser = (req,res,next)

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { updateUser, deleteUser, getUser, getAllUsers } from "../controllers/user.js";
 import { verifyToken } from "../utils/verifyToken.js";
+import { verifyUser } from "../utils/verifyToken.js";
 
 
 const router = express.Router();
 
 router.get("/checkauthentication", verifyToken, (req,res,next) => {
-    res.send("hello use, you are logged in.")
+    res.send("hello user, you are logged in.")
+} );
+
+router.get("/checkuser", verifyUser , (req,res,next) => {
+    res.send("hello user, you are logged in and .")
 } );
 
 
--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -9,10 +9,20 @@ export const verifyToken = (req,res,next) => {
 
     jwt.verify(token,process.env.JWT_SECRET, (err, user => {
         if(err)
-            return next(createError(401, "You are not authenticated."))
+            return next(createError(403, "Token is no valid."))
             req.user = user;
             next();
         
         
     }));
+};
+
+export const verifyUser = (req,res,next) => {
+    verifyToken(req,res,next, () => {
+        if(req.user.id === req.params.id || req.user.isAdmin){
+            next();
+        } else {
+            if (err) return next(createError(403, "You are not authorized."));
+        }
+    })
 };
