2b2178111e59fc8cdc73d470930c84bf99c83320
GitPit <rifflifter@gmail.com>
2022-10-20 10:04:57 -0400

    npm i cookie-parser

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -36,7 +36,7 @@ export const login = async (req,res,next) => {
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
         if(!isPasswordCorrect) return next(createError(400, "Wrong Password or username."));
 
-        const token = jwt.sign({ id:user._id, isAdmin:user.isAdmin });
+        const token = jwt.sign({ id:user._id, isAdmin:user.isAdmin }, process.env.JWT_SECRET);
 
         const {password, isAdmin, ...otherDetails} = user._doc;
         res.status(200).json({...otherDetails});
