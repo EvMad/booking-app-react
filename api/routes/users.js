5461516afc13006bdc2c3750efac56703d3ea4db
GitPit <rifflifter@gmail.com>
2022-10-20 11:07:24 -0400

    router.get("/checkadmin/:id", verifyAdmin ,

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { updateUser, deleteUser, getUser, getAllUsers } from "../controllers/user.js";
-import { verifyToken, verifyUser } from "../utils/verifyToken.js";
+import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 
 
 
@@ -10,8 +10,12 @@ router.get("/checkauthentication", verifyToken, (req,res,next) => {
     res.send("hello user, you are logged in.")
 } );
 
-router.get("/checkuser", verifyUser , (req,res,next) => {
+router.get("/checkuser/:id", verifyUser , (req,res,next) => {
     res.send("hello user, you are logged in and you can delete your account.")
+} );
+
+router.get("/checkadmin/:id", verifyAdmin , (req,res,next) => {
+    res.send("hello admin, you are logged in and you can delete your account.")
 } );
 
 
