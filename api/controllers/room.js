057ea5dfaf18362aae4dc47b3257102009bd002f
GitPit <rifflifter@gmail.com>
2022-10-21 11:00:55 -0400

    copied get and get all func from hotel

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -2,6 +2,8 @@ import Room from "../models/Room.js";
 import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
 
+
+//CREATE
 export const createRoom = async (req,res,next) => {
 
 const hotelId = req.params.hotelId;
@@ -45,6 +47,31 @@ export const deleteRoom = async (req, res, next) => {
     try{
         await Room.findByIdAndDelete(req.params.id);
         res.status(200).json("Room has been deleted.");
+    }catch(err){
+        next(err);
+    }
+
+};
+
+//GET
+export const getRoom = async (req, res, next) => {
+
+    try{
+        const room = await Room.findById(req.params.id);
+        res.status(200).json(room);
+    }catch(err){
+        next(err);
+    }
+
+};
+
+//GET ALL
+export const getAllRooms = async (req, res, next) => {
+ 
+    
+    try{
+        const rooms = await Room.find();
+        res.status(200).json(rooms);
     }catch(err){
         next(err);
     }
