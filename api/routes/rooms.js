4d654fa396dcfa9f70f59ea33a453ae9d6db5694
GitPit <rifflifter@gmail.com>
2022-10-21 11:06:40 -0400

    copied CRUD routes from hotel controller

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,7 +1,27 @@
 import express from "express";
-import { createRoom } from "../controllers/room.js";
+import { createRoom, updateRoom, deleteRoom } from "../controllers/room.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+//CREATE
+
+router.post("/", verifyAdmin, createRoom );
+
+//UPDATE
+
+router.put("/:id", verifyAdmin, updateRoom );
+
+//DELETE
+
+router.delete("/:id", verifyAdmin, deleteRoom );
+
+//GET
+
+router.get("/:id", getRoom );
+
+//GET ALL
+
+router.get("/", getAllRooms );
 
 export default router;
