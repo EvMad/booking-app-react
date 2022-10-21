cd50d9ef21ab9c1559bfa957dd051eb7f7f469c6
GitPit <rifflifter@gmail.com>
2022-10-21 11:04:34 -0400

    import { createRoom } / { verifyAdmin }

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,7 @@
 import express from "express";
+import { createRoom } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-export default router
+export default router;
