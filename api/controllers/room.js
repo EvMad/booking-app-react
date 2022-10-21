dacb80fe54016265ce2ec3e18b2fb5e89be7d289
GitPit <rifflifter@gmail.com>
2022-10-21 14:44:45 -0400

    corrected type-o in createRoom

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -6,7 +6,7 @@ import { createError } from "../utils/error.js";
 //CREATE
 export const createRoom = async (req,res,next) => {
 
-const hotelId = req.params.hotelId;
+const hotelId = req.params.hotelid;
 const newRoom = new Room(req.body);
 
     try{
