91de6d312068fa7a443afc4f652c08a0f5dfdffd
GitPit <rifflifter@gmail.com>
2022-10-24 12:19:30 -0400

    import Navbar from "../../components/navbar/Navbar

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
+import Navbar from "../../components/navbar/Navbar.jsx";
 import "./home.css";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <Navbar />
+        </div>
     )
 };
 
