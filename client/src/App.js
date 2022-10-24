c6270ca58308aa421730089bdf7cc9075935f72b
GitPit <rifflifter@gmail.com>
2022-10-24 12:07:55 -0400

    import Hotel from "./pages/hotel/Hotel.jsx";

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import List from "./pages/list/List.jsx";
+import Hotel from "./pages/hotel/Hotel.jsx";
 
 function App() {
   return (
