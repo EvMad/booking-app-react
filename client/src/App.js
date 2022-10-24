ffd01fe85e812fd051f8b54971765875fcab42b4
GitPit <rifflifter@gmail.com>
2022-10-24 12:04:20 -0400

    <Route path="/hotels" element={<List/>}/>

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
+import List from "./pages/list/List.jsx";
 
 function App() {
   return (
   <BrowserRouter>
   <Routes>
     <Route path="/" element={<Home/>}/>
+    <Route path="/hotels" element={<List/>}/>
   </Routes>
   </BrowserRouter>
   );
