23038a33016bf2c0fb6e0fa641254f69d2e296f1
GitPit <rifflifter@gmail.com>
2022-10-24 11:48:33 -0400

    <BrowserRouter><Routes><Route>

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 
 function App() {
   return (
- 
- <div></div>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/"></Route>
+  </Routes>
+  </BrowserRouter>
   );
 }
 
