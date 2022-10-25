c8ef0366fd55c19927281f8e54d1735fff1f65c2
GitPit <rifflifter@gmail.com>
2022-10-25 11:39:49 -0400

    import Header from "../../components/header/Header

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../../components/navbar/Navbar.jsx";
+import Header from "../../components/header/Header.jsx";
 import "./home.css";
 
 const Home = () => {
     return (
         <div>
             <Navbar />
+            <Header />
         </div>
     )
 };
