850855a596c0b5d58ca156f7f7ddef52ff346de4
GitPit <rifflifter@gmail.com>
2022-10-26 15:27:20 -0400

    <div className="headerSearch">< "headerSearchItem"

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -35,6 +35,9 @@ const Header = () => {
                 <h1 classname="headerTitle">A Lifetime of discounts? It's Genius</h1>
                 <p className="headerDesc">Get rewarded for your travels - unlock instant savings of 10% or more with a free Booking account</p>
                 <button className="headerBtn">Sign In / Register</button>
+                <div className="headerSearch">
+                    <div className="headerSearchItem"></div>
+                </div>
                 </div>
                 </div>
          
