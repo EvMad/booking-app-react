7d45127282fb8a31ed624722320b79869ad450c9
GitPit <rifflifter@gmail.com>
2022-10-26 15:19:50 -0400

    <button className="headerBtn">Sign In / Register

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -34,6 +34,7 @@ const Header = () => {
                 </div>
                 <h1 classname="headerTitle">A Lifetime of discounts? It's Genius</h1>
                 <p className="headerDesc">Get rewarded for your travels - unlock instant savings</p>
+                <button className="headerBtn">Sign In / Register</button>
                 </div>
                 </div>
          
