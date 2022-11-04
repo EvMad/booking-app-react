1ea40fd91b7cb370b707512e09c2cf867bb86a7b
GitPit <rifflifter@gmail.com>
2022-11-04 11:34:23 -0400

    created images.jsx to export images from folder

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,5 +1,6 @@
 import "./featured.css";
-import Chicago from "../../images/Chicago.jpg";
+import { Chicago, NYC, Tokyo } from "../../images/images.jsx";
+
 
 const Featured = () => {
     return (
@@ -8,6 +9,21 @@ const Featured = () => {
                 <img src={Chicago} alt="Chicago skyline at sunset" className="featuredImg" />
                 <div className="featuredTitles">
                     <h1>Chicago</h1>
+                    <h2>LakeShore Properties</h2>
+                </div>
+            </div>
+            <div className="featuredItem">
+                <img src={NYC} alt="Manhattan skyline" className="featuredImg" />
+                <div className="featuredTitles">
+                    <h1>New York</h1>
+                    <h2>Manhattan Properties</h2>
+                </div>
+            </div>
+            <div className="featuredItem">
+                <img src={Tokyo} alt="Tokyo Skyline at night" className="featuredImg" />
+                <div className="featuredTitles">
+                    <h1>Tokyo</h1>
+                    <h2>Mt. Fuji Properties</h2>
                 </div>
             </div>
         </div>
---
+++ client/src/images/images.jsx
@@ -0,0 +1,10 @@
+import Chicago from './Chicago.jpg';
+import NYC from './NYC.jpg';
+import Tokyo from './Tokyo.jpg';
+
+
+export {
+    Chicago,
+    NYC,
+    Tokyo
+};
