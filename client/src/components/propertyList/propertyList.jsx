1df2d70f308f3f9a7a783d3a42f550fa54f94ff7
GitPit <rifflifter@gmail.com>
2022-11-04 12:26:42 -0400

    .pListTitles>h1 {} .pListTitles>h2 {}

--- client/src/components/propertyList/propertyList.jsx
+++ client/src/components/propertyList/propertyList.jsx
@@ -11,13 +11,13 @@ const PropertyList = () => {
                 <img src={Villa} alt="Villa with glass sliding doors and swimming pool" className="pListImg" />
                 <div className="pListTitles">
                     <h1>Villas</h1>
-                    <h2>579 villas</h2>
+                    <h2>9579 villas</h2>
                 </div>
                 </div>
                 <div className="pListItem">
                 <img src={Apartment} alt="Apartment interior with yellow sofa" className="pListImg" />
                 <div className="pListTitles">
-                    <h1>Apartment</h1>
+                    <h1>Apartments</h1>
                     <h2>23,564 apartments</h2>
                 </div>
             </div>
