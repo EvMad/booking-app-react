91f125307eb8077258ce20129f7d58d25ef15a31
GitPit <rifflifter@gmail.com>
2022-11-04 12:17:26 -0400

    import PropertyList / <PropertyList/>

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../../components/navbar/Navbar.jsx";
 import Header from "../../components/header/Header.jsx";
 import Featured from "../../components/featured/Featured.jsx";
+import PropertyList from "../../components/propertyList/propertyList.jsx";
 import "./home.css";
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <div className="homeContainer">
                 <Featured />
                 <h1 className="homeTitle">Browse by property type</h1>
+                <PropertyList/>
             </div>
         </div>
     )
