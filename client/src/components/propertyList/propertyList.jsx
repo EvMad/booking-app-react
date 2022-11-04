6ff5bf436665c3e6e63a9790e1c3602dfe9d213a
GitPit <rifflifter@gmail.com>
2022-11-04 12:06:58 -0400

    added Apartment image and pListItem

--- client/src/components/propertyList/propertyList.jsx
+++ client/src/components/propertyList/propertyList.jsx
@@ -1,4 +1,5 @@
 import { Villa } from "../../images/images.jsx";
+import { Apartment } from "../../images/images.jsx";
 import "./propertyList.css";
 
 const PropertyList = () => {
@@ -6,6 +7,17 @@ const PropertyList = () => {
         <div className="pList">
             <div className="pListItem">
                 <img src={Villa} alt="Villa with glass sliding doors and swimming pool" className="pListImg" />
+                <div className="pListTitles">
+                    <h1>Villas</h1>
+                    <h2>579 villas</h2>
+                </div>
+                <div className="pListItem">
+                <img src={Apartment} alt="Apartment interior with yellow sofa" className="pListImg" />
+                <div className="pListTitles">
+                    <h1>Apartment</h1>
+                    <h2>23,564 apartments</h2>
+                </div>
+            </div>
             </div>
         </div>
     );
--- client/src/images/images.jsx
+++ client/src/images/images.jsx
@@ -1,3 +1,4 @@
+import Apartment from './Apartment.jpg';
 import Chicago from './Chicago.jpg';
 import NYC from './NYC.jpg';
 import Tokyo from './Tokyo.jpg';
@@ -5,6 +6,7 @@ import Villa from './Villa.jpg';
 
 
 export {
+    Apartment,
     Chicago,
     NYC,
     Tokyo,
