07bb5c2c69a3284ed5950f59ee2c9865aef0c36c
GitPit <rifflifter@gmail.com>
2022-11-04 12:15:59 -0400

    added Resort image and pListItem

--- client/src/components/propertyList/propertyList.jsx
+++ client/src/components/propertyList/propertyList.jsx
@@ -1,6 +1,7 @@
 import { Villa } from "../../images/images.jsx";
 import { Apartment } from "../../images/images.jsx";
 import { Hotel } from "../../images/images.jsx";
+import { Resort } from "../../images/images.jsx";
 import "./propertyList.css";
 
 const PropertyList = () => {
@@ -12,6 +13,7 @@ const PropertyList = () => {
                     <h1>Villas</h1>
                     <h2>579 villas</h2>
                 </div>
+                </div>
                 <div className="pListItem">
                 <img src={Apartment} alt="Apartment interior with yellow sofa" className="pListImg" />
                 <div className="pListTitles">
@@ -20,13 +22,20 @@ const PropertyList = () => {
                 </div>
             </div>
             <div className="pListItem">
-                <img src={Hotel} alt="Apartment interior with yellow sofa" className="pListImg" />
+                <img src={Hotel} alt="Hotel room balcony view" className="pListImg" />
                 <div className="pListTitles">
                     <h1>Hotels</h1>
                     <h2>576,284 hotels</h2>
                 </div>
             </div>
+            <div className="pListItem">
+                <img src={Resort} alt="Maldives resort bungalows on water" className="pListImg" />
+                <div className="pListTitles">
+                    <h1>Resorts</h1>
+                    <h2>76,549 resorts</h2>
+                </div>
             </div>
+            
         </div>
     );
 };
--- client/src/images/images.jsx
+++ client/src/images/images.jsx
@@ -2,6 +2,7 @@ import Apartment from './Apartment.jpg';
 import Chicago from './Chicago.jpg';
 import Hotel from './Hotel.jpg';
 import NYC from './NYC.jpg';
+import Resort from './Resort.jpg';
 import Tokyo from './Tokyo.jpg';
 import Villa from './Villa.jpg';
 
@@ -11,6 +12,7 @@ export {
     Chicago,
     Hotel,
     NYC,
+    Resort,
     Tokyo,
     Villa
 };
