fce67daa373c64f492a858e495f9b95acfebe5bd
GitPit <rifflifter@gmail.com>
2022-11-07 10:38:59 -0500

    <div className="fpRating"><button><span>

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -7,6 +7,10 @@ const FeaturedProperties = () => {
             <span className="fpName">Fountain Hotel</span>
             <span className="fpCity">Rome, Italy</span>
             <span className="fpPrice">Starting from $110</span>
+            <div className="fpRating">
+                <button>8.9</button>
+                <span>Excellent</span>
+            </div>
         </div>
     );
 };
