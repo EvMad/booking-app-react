0a4c5df8d2d2ae139cd39fec7850044e0fd84541
GitPit <rifflifter@gmail.com>
2022-11-08 13:28:53 -0500

    .fList { }

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -44,7 +44,8 @@ const Footer = () => {
                     <li className="fListItem">Airports</li>
                     <li className="fListItem">Hotels</li>
                 </ul>
-            </div>
+            </div> 
+            <div className="fText">Copyright 2022</div>
         </div>
     );
 };
