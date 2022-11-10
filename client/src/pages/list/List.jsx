6843a841e7d2c10e3a1cd6973b38c561ad77866d
GitPit <rifflifter@gmail.com>
2022-11-10 18:45:11 -0500

    <div>'s for listContainer, listWrapper, etc

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -4,8 +4,16 @@ import Header from "../../components/header/Header";
 
 const List = () => {
     return (
-        <div><Navbar/>
-        <Header type="list"/></div>
+        <div>
+        <Navbar/>
+        <Header type="list"/>
+            <div className="listContainer">
+                <div className="listWrapper">
+                    <div className="listSearch"></div>
+                    <div className="listResult"></div>
+                </div>
+            </div>
+        </div>
     )
 };
 
