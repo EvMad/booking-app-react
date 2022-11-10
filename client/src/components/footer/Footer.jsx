883dd269c81593668f92995ddd04fa497088eb36
GitPit <rifflifter@gmail.com>
2022-11-10 14:53:17 -0500

    updated footer list text

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -13,12 +13,12 @@ const Footer = () => {
                     <li className="fListItem">Hotels</li>
                 </ul>
                 <ul className="fList">
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Regions</li>
-                    <li className="fListItem">Cities</li>
-                    <li className="fListItem">Districts</li>
-                    <li className="fListItem">Airports</li>
-                    <li className="fListItem">Hotels</li>
+                    <li className="fListItem">Homes</li>
+                    <li className="fListItem">Apartments</li>
+                    <li className="fListItem">Resorts</li>
+                    <li className="fListItem">Villas</li>
+                    <li className="fListItem">Hostels</li>
+                    <li className="fListItem">Guest Houses</li>
                 </ul>
                 <ul className="fList">
                     <li className="fListItem">Countries</li>
