aedcaf9e5e66648616824aa1aebd46e756737855
GitPit <rifflifter@gmail.com>
2022-11-10 19:11:53 -0500

    import { useNavigate } from "react-router-dom";

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -12,6 +12,7 @@ import { useState } from "react";
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
 import { format } from "date-fns";
+import { useNavigate } from "react-router-dom";
 
 const Header = ({type}) => {
     const [destination, setDestination] = useState("");
