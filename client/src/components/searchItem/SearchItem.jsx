264bf6eedeca53f162216e19042fc57967041a92
GitPit <rifflifter@gmail.com>
2022-11-11 16:31:13 -0500

    <div className="siDetails"></div>

--- client/src/components/searchItem/SearchItem.jsx
+++ client/src/components/searchItem/SearchItem.jsx
@@ -4,9 +4,9 @@ import { Search1 } from "../../images/images.jsx";
 const SearchItem = () => {
     return (
         <div className="searchItem">
-            <img src={ Search1 } alt="" className="siImg" />
+            <img src={ Search1 } alt="View from Coastal Italian Hotel Room" className="siImg" />
             <div className="siDesc"></div>
-            <div className="siDesc"></div>
+            <div className="siDetails"></div>
         </div>
     );
 };
