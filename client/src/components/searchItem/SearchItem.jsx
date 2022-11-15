5e0682a8c2866a3cd1e34a7fe302d17a0955e773
GitPit <rifflifter@gmail.com>
2022-11-15 15:08:50 -0500

    .siDetails { }

--- client/src/components/searchItem/SearchItem.jsx
+++ client/src/components/searchItem/SearchItem.jsx
@@ -6,7 +6,7 @@ const SearchItem = () => {
         <div className="searchItem">
             <img src={ Search1 } alt="View from Coastal Italian Hotel Room" className="siImg" />
             <div className="siDesc">
-                <h1 className="siTitle">Amalfi Suites</h1>
+                <h1 className="siTitle">Oia Suites</h1>
                 <span className="siDistance">0.1mi from center</span>
                 <span className="siTaxiOp">Free airport shuttle</span>
                 <span className="siSubtitle">
