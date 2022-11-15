8ed463bd73336f22464c01490081f36512acbcdf
GitPit <rifflifter@gmail.com>
2022-11-15 14:45:47 -0500

    <span className="siTaxiOp">

--- client/src/components/searchItem/SearchItem.jsx
+++ client/src/components/searchItem/SearchItem.jsx
@@ -8,6 +8,7 @@ const SearchItem = () => {
             <div className="siDesc">
                 <h1 className="siTitle">South Gaul Apartments</h1>
                 <span className="siDistance">0.7mi from center</span>
+                <span className="siTaxiOp">Free airport shuttle</span>
             </div>
             <div className="siDetails"></div>
         </div>
