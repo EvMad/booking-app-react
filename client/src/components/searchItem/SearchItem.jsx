f0f0fb49204cec5353daa42ce347c0d5d1d98b39
GitPit <rifflifter@gmail.com>
2022-11-15 14:47:42 -0500

    <span className="siSubtitle">

--- client/src/components/searchItem/SearchItem.jsx
+++ client/src/components/searchItem/SearchItem.jsx
@@ -6,9 +6,12 @@ const SearchItem = () => {
         <div className="searchItem">
             <img src={ Search1 } alt="View from Coastal Italian Hotel Room" className="siImg" />
             <div className="siDesc">
-                <h1 className="siTitle">South Gaul Apartments</h1>
-                <span className="siDistance">0.7mi from center</span>
+                <h1 className="siTitle">Amalfi Suites</h1>
+                <span className="siDistance">0.1mi from center</span>
                 <span className="siTaxiOp">Free airport shuttle</span>
+                <span className="siSubtitle">
+                    One Bedroom Apartment with Balcony
+                </span>
             </div>
             <div className="siDetails"></div>
         </div>
