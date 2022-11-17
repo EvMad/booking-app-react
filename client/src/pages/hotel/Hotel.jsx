ea901d7e540896b32797f48d9cf222186d6f05f6
GitPit <rifflifter@gmail.com>
2022-11-17 14:12:32 -0500

    import Header <Header type="list"/>

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -1,10 +1,12 @@
 import './hotel.css';
 import Navbar from "../../components/navbar/Navbar.jsx";
+import Header from "../../components/header/Header.jsx";
 
 const Hotel = () => {
     return (
         <div>
             <Navbar/>
+            <Header type="list"/>
         </div>
     )
 };
