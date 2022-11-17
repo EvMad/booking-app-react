0ed437e6b95049a707cf753682d0261130838f90
GitPit <rifflifter@gmail.com>
2022-11-17 14:21:36 -0500

    <span className="hotelPriceHighlight">

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -14,7 +14,10 @@ const Hotel = () => {
                     <h1 className="hotelTitle">Grand Hotel</h1>
                     <div className="hotelAddress">
                         <FontAwesomeIcon icon={faLocationDot}/>
+                        <span>567 Grand Ave Los Angeles</span>
                     </div>
+                    <span className="hotelDistance">Excellent Location - .5mi from center</span>
+                    <span className="hotelPriceHighlight">Book a stay over $200 at this property and get a free airport taxi</span>
                 </div>
             </div>
         </div>
