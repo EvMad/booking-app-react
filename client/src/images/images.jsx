a5190833e407243768c4c9b06087693def36214e
GitPit <rifflifter@gmail.com>
2022-11-17 14:44:13 -0500

    created array of new images to export

--- client/src/images/images.jsx
+++ client/src/images/images.jsx
@@ -5,11 +5,19 @@ import Feature2 from './Feature2.jpg';
 import Feature3 from './Feature3.jpg';
 import Feature4 from './Feature4.jpg';
 import Hotel from './Hotel.jpg';
+import Interior1 from './Interior1.jpg';
+import Interior2 from './Interior2.jpg';
+import Interior3 from './Interior3.jpg';
+import Interior4 from './Interior4.jpg';
+import Interior5 from './Interior5.jpg';
+import Interior6 from './Interior6.jpg';
 import NYC from './NYC.jpg';
 import Resort from './Resort.jpg';
 import Search1 from './Search1.jpg';
 import Tokyo from './Tokyo.jpg';
 import Villa from './Villa.jpg';
+
+const roomArray = [ Interior1, Interior2, Interior3, Interior4, Interior5, Interior6 ];
 
 
 export {
@@ -22,6 +30,7 @@ export {
     Hotel,
     NYC,
     Resort,
+    roomArray,
     Search1,
     Tokyo,
     Villa
