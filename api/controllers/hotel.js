3d15f5540818884d82de37a9c2cd156d3bce9a08
GitPit <rifflifter@gmail.com>
2022-11-25 17:38:12 -0500

    return Hotel.countDocuments({city:city})

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -71,7 +71,7 @@ export const countByCity = async (req, res, next) => {
     const cities = req.query.cities.split(",")  
     try{
         const list = await Promise.all(cities.map(city => {
-            return Hotel.find({city:city}).length
+            return Hotel.countDocuments({city:city})
         }))
         res.status(200).json(hotels);
     }catch(err){
