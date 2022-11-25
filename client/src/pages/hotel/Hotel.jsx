784fdba9525faeb1c54b7f94b8c416b7161c40c5
GitPit <rifflifter@gmail.com>
2022-11-25 17:21:42 -0500

    const handleMove = (direction) =>

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -20,7 +20,18 @@ const Hotel = () => {
         setSlideNumber(i);
         setOpen(true);
 
-    }
+    };
+
+    const handleMove = (direction) => {
+        let newSlideNumber;
+
+        if(direction === "l") {
+            newSlideNumber = slideNumber === 0 ? 5 : slideNumber -1
+        }else{
+            newSlideNumber = slideNumber === 5 ? 0 : slideNumber +1
+        }
+        setSlideNumber(newSlideNumber)
+    };
 
     return (
         <div>
@@ -29,11 +40,11 @@ const Hotel = () => {
             <div className="hotelContainer">
                {open &&  <div className="slider">
                 <FontAwesomeIcon icon={faCircleXmark} className="close" onClick={()=>setOpen(false)}/>
-                <FontAwesomeIcon icon={faCircleArrowLeft} className="arrow" onClick={()=>handleMove("")}/>
+                <FontAwesomeIcon icon={faCircleArrowLeft} className="arrow" onClick={()=>handleMove("l")}/>
                 <div className="sliderWrapper">
                 <img src={roomArray[slideNumber]} alt="" className="sliderImg" />
                 </div>
-                <FontAwesomeIcon icon={faCircleArrowRight} className="arrow" onClick={()=>handleMove("")/>
+                <FontAwesomeIcon icon={faCircleArrowRight} className="arrow" onClick={()=>handleMove("r")}/>
                 </div>}
                 <div className="hotelWrapper">
                     <button className="bookNow">Reserve or Book Now!</button>
