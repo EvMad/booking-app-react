16f21861011effe2691df980b2676c121c84e303
GitPit <rifflifter@gmail.com>
2022-11-29 16:07:14 -0500

    useEffect(() => { const fetchData = async

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -4,4 +4,13 @@ const useFetch = (url) => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
     const [ error, setError] = useState(false);
-};
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true)
+            const res = axios.get(url)
+        }
+    }, [url])
+}; 
+
+
