ef0f33cb3be4de3872585de4a02766259fd23a75
GitPit <rifflifter@gmail.com>
2022-11-30 14:33:21 -0500

    {loading ? ( "Loading please wait" ) : (

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -10,27 +10,29 @@ const Featured = () => {
 
     return (
         <div className="featured">
-            <div className="featuredItem">
-                <img src={Chicago} alt="Chicago skyline at sunset" className="featuredImg" />
-                <div className="featuredTitles">
-                    <h1>Chicago</h1>
-                    <h2>LakeShore Properties</h2>
-                </div>
-            </div>
-            <div className="featuredItem">
-                <img src={NYC} alt="Manhattan skyline" className="featuredImg" />
-                <div className="featuredTitles">
-                    <h1>New York</h1>
-                    <h2>Manhattan Properties</h2>
-                </div>
-            </div>
-            <div className="featuredItem">
-                <img src={Tokyo} alt="Tokyo Skyline at night" className="featuredImg" />
-                <div className="featuredTitles">
-                    <h1>Tokyo</h1>
-                    <h2>Mt. Fuji Properties</h2>
-                </div>
-            </div>
+    {loading ? ( "Loading please wait" ) : (
+        <>        <div className="featuredItem">
+        <img src={Chicago} alt="Chicago skyline at sunset" className="featuredImg" />
+        <div className="featuredTitles">
+            <h1>Chicago</h1>
+            <h2>LakeShore Properties</h2>
+        </div>
+    </div>
+    <div className="featuredItem">
+        <img src={NYC} alt="Manhattan skyline" className="featuredImg" />
+        <div className="featuredTitles">
+            <h1>New York</h1>
+            <h2>Manhattan Properties</h2>
+        </div>
+    </div>
+    <div className="featuredItem">
+        <img src={Tokyo} alt="Tokyo Skyline at night" className="featuredImg" />
+        <div className="featuredTitles">
+            <h1>Tokyo</h1>
+            <h2>Mt. Fuji Properties</h2>
+        </div>
+    </div></>
+    )}
         </div>
     );
 };
