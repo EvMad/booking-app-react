dd9a9148c786bb30e419fa75f70fbf63c3424dba
GitPit <rifflifter@gmail.com>
2022-12-01 14:23:37 -0500

    removed 3 of 4 pListItem cards

--- client/src/components/propertyList/propertyList.jsx
+++ client/src/components/propertyList/propertyList.jsx
@@ -21,27 +21,7 @@ const PropertyList = () => {
                     <h2>9579 villas</h2>
                 </div>
                 </div>
-                <div className="pListItem">
-                <img src={Apartment} alt="Apartment interior with yellow sofa" className="pListImg" />
-                <div className="pListTitles">
-                    <h1>Apartments</h1>
-                    <h2>23,564 apartments</h2>
-                </div>
-            </div>
-            <div className="pListItem">
-                <img src={Hotel} alt="Hotel room balcony view" className="pListImg" />
-                <div className="pListTitles">
-                    <h1>Hotels</h1>
-                    <h2>576,284 hotels</h2>
-                </div>
-            </div>
-            <div className="pListItem">
-                <img src={Resort} alt="Maldives resort bungalows on water" className="pListImg" />
-                <div className="pListTitles">
-                    <h1>Resorts</h1>
-                    <h2>76,549 resorts</h2>
-                </div>
-            </div>
+           
        </>)}
             
         </div>
