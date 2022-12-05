956d838a4ccef16d4822b79915950b62b1355280
GitPit <rifflifter@gmail.com>
2022-12-05 13:31:13 -0500

    cheapestPrice: { $gt: min | 1, $lt: max || 9999},

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -59,7 +59,9 @@ export const getAllHotels = async (req, res, next) => {
     const { min, max, ...others } = req.query;
     
     try{
-        const hotels = await Hotel.find({...others}).limit(req.query.limit);
+        const hotels = await Hotel.find({...others, 
+        cheapestPrice: { $gt: min | 1, $lt: max || 9999},
+        }).limit(req.query.limit);
         res.status(200).json(hotels);
     }catch(err){
         next(err);
