7e45aec60474d386acef662fbb392bd91389322c
GitPit <rifflifter@gmail.com>
2022-12-09 15:50:57 -0500

    <h1 className="hotelTitle">{data.name}</h1>

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -54,7 +54,7 @@ const Hotel = () => {
                 </div>}
                 <div className="hotelWrapper">
                     <button className="bookNow">Reserve or Book Now!</button>
-                    <h1 className="hotelTitle">Grand Hotel</h1>
+                    <h1 className="hotelTitle">{data.name}</h1>
                     <div className="hotelAddress">
                         <FontAwesomeIcon icon={faLocationDot}/>
                         <span>567 Grand Ave Los Angeles</span>
