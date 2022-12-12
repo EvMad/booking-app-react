454d6168910eebe3c5a6694b91b03a6f5fca0ee1
GitPit <rifflifter@gmail.com>
2022-12-12 12:05:37 -0500

    <h1 className="hotelTitle">{data.title}</h1>

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -70,7 +70,7 @@ const Hotel = () => {
                     </div>
                     <div className="hotelDetails">
                         <div className="hotelDetailsText">
-                            <h1 className="hotelTitle">Stay in the heart of Los Angeles</h1>
+                            <h1 className="hotelTitle">{data.title}</h1>
                             <p className="hotelDesc">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
                             </p></div>
                         <div className="hotelDetailsPrice">
