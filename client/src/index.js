50ccf644cf75f721920758404c56ba1be55e321f
GitPit <rifflifter@gmail.com>
2023-01-26 13:48:44 -0500

    wrapped app in <AuthContextProvider>

--- client/src/index.js
+++ client/src/index.js
@@ -8,9 +8,11 @@ import { SearchContextProvider } from './context/SearchContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SearchContextProvider>
-    <App />
-    </SearchContextProvider>
+    <AuthContextProvider>
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
