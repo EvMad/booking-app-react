d2ab39af0740a7d67e094a54d52f3b21077d1030
GitPit <rifflifter@gmail.com>
2023-02-06 13:10:38 -0500

    const [ credentials, setCredentials ] = useState({

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -2,6 +2,12 @@ import { useState } from "react";
 import "./login.css";
 
 const Login = () => {
+
+    const [ credentials, setCredentials ] = useState({
+        username: undefined,
+        password: undefined,
+    })
+
     return (
         <div>Login</div>
     )
