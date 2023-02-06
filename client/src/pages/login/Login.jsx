e3ab049799c7d9eae9f82957d30fdbedf78a6c3a
GitPit <rifflifter@gmail.com>
2023-02-06 13:28:38 -0500

    wrote async function for handleClick

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState, useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import "./login.css";
@@ -15,16 +16,27 @@ const Login = () => {
         setCredentials((prev) => ({...prev, [e.target.id]: e.target.value}));
     };
 
+    const handleClick = async (e) => {
+        e.preventDefault();
+        dispatch({type:"LOGIN_START"});
+        try {
+            const res = await axios.post("/auth/login", credentials);
+            dispatch({type: "LOGIN_SUCCESS", payload: res.data});
+        } catch (err) {
+            dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+        }
+    };
+
     return (
         <div className="login">
             <div className="lContainer">
                 <input type="text" placeholder="username" id="username" onChange={handleChange} className="lInput"></input>
                 <input type="password" placeholder="password" id="password" onChange={handleChange} className="lInput"></input>
-                <button className="lButton">Login</button>
+                <button onClick={handleClick} className="lButton">Login</button>
                 {error && <span>{error.message}</span>}
             </div>
         </div>
     )
 };
 
-export default Login 
+export default Login;
