fddc9a20766f897a34e6702290ebd6fa92096cba
GitPit <rifflifter@gmail.com>
2023-03-02 13:57:23 -0500

    <div className="rPrice">{item.price}</div>

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -21,6 +21,7 @@ const Reserve = ({setOpen, hotelId}) => {
                             <div className="rTitle">{item.title}</div>
                             <div className="rDesc">{item.desc}</div>
                             <div className="rMax">Max People: <b>{item.maxPeople}</b></div>
+                            <div className="rPrice">{item.price}</div>
                         </div>
                     </div>
                 ))}
