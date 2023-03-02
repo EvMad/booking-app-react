e6be80ebd27778c4ed303e378c3b388ced1c3fa9
GitPit <rifflifter@gmail.com>
2023-03-02 15:09:49 -0500

    "roomNumbers.$.unavailableDates": req.body.dates

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -49,7 +49,7 @@ export const updateRoomAvailability = async (req, res, next) => {
             { "roomNumbers._id": req.params.id }, 
         {
             $push: {
-                "roomNumbers"
+                "roomNumbers.$.unavailableDates": req.body.dates
             },
         }
         );
