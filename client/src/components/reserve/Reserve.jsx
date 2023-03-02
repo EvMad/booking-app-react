e1c80d7637de1adf0841db1a9312bd78ea309699
GitPit <rifflifter@gmail.com>
2023-03-02 13:55:37 -0500

    <div className="rMax">Max People: <b>{item.maxPeop

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -20,7 +20,7 @@ const Reserve = ({setOpen, hotelId}) => {
                         <div className="rItemInfo">
                             <div className="rTitle">{item.title}</div>
                             <div className="rDesc">{item.desc}</div>
-                            <div className="rMax">{item.maxPeople}</div>
+                            <div className="rMax">Max People: <b>{item.maxPeople}</b></div>
                         </div>
                     </div>
                 ))}
