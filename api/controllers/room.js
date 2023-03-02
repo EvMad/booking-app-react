f27de6537ebd52ecbef781e4f52734800ffcda5e
GitPit <rifflifter@gmail.com>
2023-03-02 15:07:55 -0500

    await Room.updateOne({ "roomNumbers._id": req.par

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -45,12 +45,14 @@ export const updateRoom = async (req, res, next) => {
 export const updateRoomAvailability = async (req, res, next) => {
 
     try{
-
-        const updatedRoom = await Room.findByIdAndUpdate(
-            req.params.id, 
-            { $set: req.body }, 
-            { new: true } 
-            );
+        await Room.updateOne(
+            { "roomNumbers._id": req.params.id }, 
+        {
+            $push: {
+                "roomNumbers"
+            },
+        }
+        );
         res.status(200).json(updatedRoom);
     }catch(err){
         next(err);
