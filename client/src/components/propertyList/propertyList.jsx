7cc28314ff0f000e1146b2c4bbe594554ec0231c
GitPit <rifflifter@gmail.com>
2023-03-23 11:23:19 -0400

    added static div's for property type features

--- client/src/components/propertyList/propertyList.jsx
+++ client/src/components/propertyList/propertyList.jsx
@@ -11,14 +11,49 @@ const PropertyList = () => {
 
     const images = [{Villa, Apartment, Hotel, Resort}];
 
+    var randomPhoto = images[Math.floor(Math.random() * images.length)];
+
     return (
         <div className="pList">
+
+<div className="pListItem">
+         <img src={Villa} alt="Villa with glass sliding doors and swimming pool" className="pListImg" />
+         <div className="pListTitles">
+             <h1>Villas</h1>
+             <h2>Explore More Available Villas</h2>
+         </div>
+         </div>
+
+         <div className="pListItem">
+         <img src={Apartment} alt="Apartment Living Room" className="pListImg" />
+         <div className="pListTitles">
+             <h1>Apartments</h1>
+             <h2>Explore More Available Apartments</h2>
+         </div>
+         </div>
+
+         <div className="pListItem">
+         <img src={Hotel} alt="Hotel Balcony" className="pListImg" />
+         <div className="pListTitles">
+             <h1>Hotels</h1>
+             <h2>Explore More Available Hotels</h2>
+         </div>
+         </div>
+
+         <div className="pListItem">
+         <img src={Resort} alt="Oceanside bungalows" className="pListImg" />
+         <div className="pListTitles">
+             <h1>Resorts</h1>
+             <h2>Explore More Available Resorts</h2>
+         </div>
+         </div>
+
        {loading ? ("loading") : (<>
 
        {data && images.map((img, i) => (   
        
         <div className="pListItem" key={i}>
-         <img src={img} alt="Villa with glass sliding doors and swimming pool" className="pListImg" />
+         <img src={Resort} alt="Villa with glass sliding doors and swimming pool" className="pListImg" />
          <div className="pListTitles">
              <h1>{data[i]?.type}</h1>
              <h2>{data[i]?.count} {data[i]?.type}</h2>
