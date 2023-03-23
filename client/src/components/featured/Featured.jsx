85cdf63e10741c069fa8ef02af1a00b046b68e1b
GitPit <rifflifter@gmail.com>
2023-03-23 10:58:37 -0400

    updated featured headings text

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -14,21 +14,21 @@ const Featured = () => {
         <>        <div className="featuredItem">
         <img src={Chicago} alt="Chicago skyline at sunset" className="featuredImg" />
         <div className="featuredTitles">
-            <h1>Berlin</h1>
+            <h1>Chicago</h1>
             <h2>{data[0]} properties</h2>
         </div>
     </div>
     <div className="featuredItem">
         <img src={NYC} alt="Manhattan skyline" className="featuredImg" />
         <div className="featuredTitles">
-            <h1>Madrid</h1>
+            <h1>New York</h1>
             <h2>{data[1]} properties</h2>
         </div>
     </div>
     <div className="featuredItem">
         <img src={Tokyo} alt="Tokyo Skyline at night" className="featuredImg" />
         <div className="featuredTitles">
-            <h1>London</h1>
+            <h1>Tokyo</h1>
             <h2>{data[2]} properties</h2>
         </div>
     </div></>
